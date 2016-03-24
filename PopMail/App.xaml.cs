f5d39e4aecfcc72a511748db86a6136e45b4302a
KeesPolling <Kees_Polling@Live.com>
2016-03-24 11:10:53 +0100

    Rename EmailProxies2 step 2

---
+++ PopMail/App.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Prism.Unity.Windows;
+using Microsoft.Practices.Unity;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
+using Windows.UI.Notifications;
+using Prism.Events;
+using Prism.Mvvm;
+using Prism.Windows.AppModel;
+using Prism.Windows.Navigation;
+
+namespace PopMail
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : PrismUnityApplication
+    {
+        // Bootstrap: App singleton service declarations
+        private TileUpdater _tileUpdater;
+
+        public IEventAggregator EventAggregator { get; set; }
+
+        public App()
+        {
+            this.InitializeComponent();
+        }
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
+        {
+            NavigationService.Navigate("Main", null);
+            // Navigate to the initial page
+            NavigationService.Navigate("Hub", null);
+            Window.Current.Activate();
+            return Task.FromResult<object>(null);
+        }
+        protected override Task OnInitializeAsync(IActivatedEventArgs args)
+        {
+            EventAggregator = new EventAggregator();
+
+            Container.RegisterInstance<INavigationService>(NavigationService);
+            Container.RegisterInstance<ISessionStateService>(SessionStateService);
+            Container.RegisterInstance<IEventAggregator>(EventAggregator);
+            Container.RegisterInstance<IResourceLoader>(new ResourceLoaderAdapter(new ResourceLoader()));
+            //     Register any app specific types with the container
+
+            ViewModelLocationProvider.SetDefaultViewTypeToViewModelTypeResolver((viewType) =>
+            {
+                var viewModelTypeName = string.Format(CultureInfo.InvariantCulture, "PopMail.UILogic.ViewModels.{0}ViewModel, AdventureWorks.UILogic, Version=1.1.0.0, Culture=neutral", viewType.Name);
+                var viewModelType = Type.GetType(viewModelTypeName);
+                if (viewModelType == null)
+                {
+                    viewModelTypeName = string.Format(CultureInfo.InvariantCulture, "PopMail.UILogic.ViewModels.{0}ViewModel, PopMail.UILogic.Windows, Version=1.0.0.0, Culture=neutral", viewType.Name);
+                    viewModelType = Type.GetType(viewModelTypeName);
+                }
+
+                return viewModelType;
+            });
+
+            // Documentation on working with tiles can be found at http://go.microsoft.com/fwlink/?LinkID=288821&clcid=0x409
+            _tileUpdater = TileUpdateManager.CreateTileUpdaterForApplication();
+            //   _tileUpdater.StartPeriodicUpdate(new Uri(Constants.ServerAddress + "/api/TileNotification"), PeriodicUpdateRecurrence.HalfHour);
+            var resourceLoader = Container.Resolve<IResourceLoader>();
+
+            return base.OnInitializeAsync(args);
+        }
+
+    }
+}
